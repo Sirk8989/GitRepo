0793a06fb1094d56c4bdb0befeb82c47b44f9568
adrian <adrian@adrian-VirtualBox>
2022-03-12 11:45:43 +0100

    "corregido el encapsulamiento de la manipulacion del objeto cuenta1 para añadir el argumento cantidad, de momento sin uso.

--- CCuenta/src/main/java/cuentas/Main.java
+++ CCuenta/src/main/java/cuentas/Main.java
@@ -6,24 +6,26 @@ public class Main {
         CCuenta cuenta1;
         double saldoActual;
 
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        operativa_cuenta(0);
+    }
+
+	protected static void operativa_cuenta(float cantidad) {
+		CCuenta cuenta1;
+		double saldoActual;
+		cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
-        operativa_cuenta(cuenta1);
-    }
-
-	private static void operativa_cuenta(CCuenta cuenta1) {
-		try {
-            cuenta1.retirar(2300);
-        } catch (Exception e) {
-            System.out.print("Fallo al retirar");
-        }
         try {
-            System.out.println("Ingreso en cuenta");
-            cuenta1.ingresar(695);
-        } catch (Exception e) {
-            System.out.print("Fallo al ingresar");
-        }
+		    cuenta1.retirar(2300);
+		} catch (Exception e) {
+		    System.out.print("Fallo al retirar");
+		}
+		try {
+		    System.out.println("Ingreso en cuenta");
+		    cuenta1.ingresar(695);
+		} catch (Exception e) {
+		    System.out.print("Fallo al ingresar");
+		}
 	}
 }
